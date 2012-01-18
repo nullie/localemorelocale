a41a183f9f8dc0321eab53f33c054030a72bf6d7
Ilya Novoselov <ilya.novoselov@gmail.com>
2012-01-18 10:15:15 +0600

    Initial commit

---
+++ src/com/yourcompany/yoursetting/bundle/PluginBundleManager.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2011 two forty four a.m. LLC <http://www.twofortyfouram.com>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package com.yourcompany.yoursetting.bundle;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+
+import com.yourcompany.yoursetting.Constants;
+
+/**
+ * Class for managing the {@link com.twofortyfouram.locale.Intent#EXTRA_BUNDLE} for this plug-in.
+ */
+public final class PluginBundleManager
+{
+    /**
+     * Private constructor prevents instantiation
+     * 
+     * @throws UnsupportedOperationException because this class cannot be instantiated.
+     */
+    private PluginBundleManager()
+    {
+        throw new UnsupportedOperationException("This class is non-instantiable"); //$NON-NLS-1$
+    }
+
+    /**
+     * Type: {@code String}
+     * <p>
+     * String message to display in a Toast message.
+     */
+    public static final String BUNDLE_EXTRA_STRING_MESSAGE = "com.yourcompany.yourapp.extra.STRING_MESSAGE"; //$NON-NLS-1$
+
+    /**
+     * Method to verify the content of the bundle are correct.
+     * <p>
+     * This method will not mutate {@code bundle}.
+     * 
+     * @param bundle bundle to verify. May be null, which will always return false.
+     * @return true if the Bundle is valid, false if the bundle is invalid.
+     */
+    public static boolean isBundleValid(final Bundle bundle)
+    {
+        if (null == bundle)
+        {
+            return false;
+        }
+
+        /*
+         * Make sure the expected extras exist
+         */
+        if (!bundle.containsKey(BUNDLE_EXTRA_STRING_MESSAGE))
+        {
+            if (Constants.IS_LOGGABLE)
+            {
+                Log.e(Constants.LOG_TAG, String.format("bundle must contain extra %s", BUNDLE_EXTRA_STRING_MESSAGE)); //$NON-NLS-1$
+            }
+            return false;
+        }
+
+        /*
+         * Make sure the correct number of extras exist. Run this test after checking for specific Bundle extras above so that the
+         * error message is more useful. (E.g. the caller will see what extras are missing, rather than just a message that there
+         * is the wrong number).
+         */
+        if (bundle.keySet().size() != 1)
+        {
+            if (Constants.IS_LOGGABLE)
+            {
+                Log.e(Constants.LOG_TAG, String.format("bundle must contain 1 key, but currently contains %d keys: %s", Integer.valueOf(bundle.keySet().size()), bundle.keySet() //$NON-NLS-1$
+                                                                                                                                                                       .toString()));
+            }
+            return false;
+        }
+
+        /*
+         * Make sure the extra isn't null or empty
+         */
+        if (TextUtils.isEmpty(bundle.getString(BUNDLE_EXTRA_STRING_MESSAGE)))
+        {
+            if (Constants.IS_LOGGABLE)
+            {
+                Log.e(Constants.LOG_TAG, String.format("bundle extra %s appears to be null or empty.  It must be a non-empty string", BUNDLE_EXTRA_STRING_MESSAGE)); //$NON-NLS-1$
+            }
+            return false;
+        }
+
+        return true;
+    }
+}
---
+++ src/com/yourcompany/yoursetting/ui/EditActivity.java
@@ -0,0 +1,237 @@
+/*
+ * Copyright 2011 two forty four a.m. LLC <http://www.twofortyfouram.com>
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at <http://www.apache.org/licenses/LICENSE-2.0>
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS"
+ * BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
+package com.yourcompany.yoursetting.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.ContextThemeWrapper;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.twofortyfouram.locale.BreadCrumber;
+import com.yourcompany.yoursetting.R;
+import com.yourcompany.yoursetting.bundle.BundleScrubber;
+import com.yourcompany.yoursetting.bundle.PluginBundleManager;
+
+/**
+ * This is the "Edit" activity for a Locale Plug-in.
+ */
+public final class EditActivity extends Activity
+{
+
+    /**
+     * Help URL, used for the {@link com.twofortyfouram.locale.platform.R.id#twofortyfouram_locale_menu_help} menu item.
+     */
+    // TODO: Place a real help URL here
+    private static final String HELP_URL = "http://www.yourcompany.com/yourhelp.html"; //$NON-NLS-1$
+
+    /**
+     * Flag boolean that can only be set to true via the "Don't Save"
+     * {@link com.twofortyfouram.locale.platform.R.id#twofortyfouram_locale_menu_dontsave} menu item in
+     * {@link #onMenuItemSelected(int, MenuItem)}.
+     * <p>
+     * If true, then this {@code Activity} should return {@link Activity#RESULT_CANCELED} in {@link #finish()}.
+     * <p>
+     * If false, then this {@code Activity} should generally return {@link Activity#RESULT_OK} with extras
+     * {@link com.twofortyfouram.locale.Intent#EXTRA_BUNDLE} and {@link com.twofortyfouram.locale.Intent#EXTRA_STRING_BLURB}.
+     * <p>
+     * There is no need to save/restore this field's state when the {@code Activity} is paused.
+     */
+    private boolean mIsCancelled = false;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void onCreate(final Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        /*
+         * A hack to prevent a private serializable classloader attack
+         */
+        BundleScrubber.scrub(getIntent());
+        BundleScrubber.scrub(getIntent().getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE));
+
+        setContentView(R.layout.main);
+
+        setTitle(BreadCrumber.generateBreadcrumb(getApplicationContext(), getIntent(), getString(R.string.plugin_name)));
+
+        final FrameLayout frame = (FrameLayout) findViewById(R.id.frame);
+        frame.addView(getLayoutInflater().cloneInContext(new ContextThemeWrapper(this, R.style.Theme_Locale_Light)).inflate(R.layout.frame, frame, false));
+
+        /*
+         * if savedInstanceState is null, then then this is a new Activity instance and a check for EXTRA_BUNDLE is needed
+         */
+        if (null == savedInstanceState)
+        {
+            final Bundle forwardedBundle = getIntent().getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
+
+            if (PluginBundleManager.isBundleValid(forwardedBundle))
+            {
+                ((EditText) findViewById(R.id.message)).setText(forwardedBundle.getString(PluginBundleManager.BUNDLE_EXTRA_STRING_MESSAGE));
+            }
+        }
+        /*
+         * if savedInstanceState != null, there is no need to restore any Activity state directly via onSaveInstanceState()), as
+         * the TextView object handles that automatically
+         */
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void finish()
+    {
+        if (mIsCancelled)
+        {
+            setResult(RESULT_CANCELED);
+        }
+        else
+        {
+            final String message = ((EditText) findViewById(R.id.message)).getText().toString();
+
+            /*
+             * If the message is of 0 length, then there isn't a setting to save.
+             */
+            if (0 == message.length())
+            {
+                setResult(RESULT_CANCELED);
+            }
+            else
+            {
+                /*
+                 * This is the return Intent, into which we'll put all the required extras
+                 */
+                final Intent returnIntent = new Intent();
+
+                /*
+                 * This extra is the data to ourselves: either for the Activity or the BroadcastReceiver. Note that anything
+                 * placed in this Bundle must be available to Locale's class loader. So storing String, int, and other standard
+                 * objects will work just fine. However Parcelable objects must also be Serializable. And Serializable objects
+                 * must be standard Java objects (e.g. a private subclass to this plug-in cannot be stored in the Bundle, as
+                 * Locale's classloader will not recognize it).
+                 */
+                final Bundle returnBundle = new Bundle();
+                returnBundle.putString(PluginBundleManager.BUNDLE_EXTRA_STRING_MESSAGE, message);
+
+                returnIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE, returnBundle);
+
+                /*
+                 * This is the blurb concisely describing what your setting's state is. This is simply used for display in the UI.
+                 */
+                if (message.length() > getResources().getInteger(R.integer.twofortyfouram_locale_maximum_blurb_length))
+                {
+                    returnIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_STRING_BLURB, message.substring(0, getResources().getInteger(R.integer.twofortyfouram_locale_maximum_blurb_length)));
+                }
+                else
+                {
+                    returnIntent.putExtra(com.twofortyfouram.locale.Intent.EXTRA_STRING_BLURB, message);
+                }
+
+                setResult(RESULT_OK, returnIntent);
+            }
+        }
+
+        super.finish();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu)
+    {
+        super.onCreateOptionsMenu(menu);
+
+        /*
+         * inflate the default menu layout from XML
+         */
+        getMenuInflater().inflate(R.menu.twofortyfouram_locale_help_save_dontsave, menu);
+
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean onMenuItemSelected(final int featureId, final MenuItem item)
+    {
+
+        /*
+         * Royal pain in the butt to support the home button in SDK 11's ActionBar
+         */
+        if (Build.VERSION.SDK_INT >= 11)
+        {
+            try
+            {
+                if (item.getItemId() == android.R.id.class.getField("home").getInt(null)) //$NON-NLS-1$
+                {
+                    // app icon in Action Bar clicked; go home
+                    final Intent intent = new Intent(getPackageManager().getLaunchIntentForPackage(getCallingPackage()));
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    finish();
+                    return true;
+                }
+            }
+            catch (final NoSuchFieldException e)
+            {
+                // this should never happen on SDK 11 or greater
+                throw new RuntimeException(e);
+            }
+            catch (final IllegalAccessException e)
+            {
+                // this should never happen on SDK 11 or greater
+                throw new RuntimeException(e);
+            }
+        }
+
+        switch (item.getItemId())
+        {
+            case R.id.twofortyfouram_locale_menu_help:
+            {
+                try
+                {
+                    startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse(HELP_URL)));
+                }
+                catch (final Exception e)
+                {
+                    Toast.makeText(getApplicationContext(), com.twofortyfouram.locale.platform.R.string.twofortyfouram_locale_application_not_available, Toast.LENGTH_LONG).show();
+                }
+
+                return true;
+            }
+            case R.id.twofortyfouram_locale_menu_dontsave:
+            {
+                mIsCancelled = true;
+                finish();
+                return true;
+            }
+            case R.id.twofortyfouram_locale_menu_save:
+            {
+                finish();
+                return true;
+            }
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
